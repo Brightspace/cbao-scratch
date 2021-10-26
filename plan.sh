414b69925fb72b801d438eea0483d7e9a55e4b51
Chenfeng Bao <chenfeng.bao@d2l.com>
2021-10-26 00:09:29 -0400

    eol

--- plan.sh
+++ plan.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat "${GITHUB_WORKSPACE}/plan.txt" | sed --silent '/=========/,$p'
+cat "${GITHUB_WORKSPACE}/plan.txt" | sed --silent '/=========/,$pa\'
 PLAN_JSON=$(cat "${GITHUB_WORKSPACE}/plan.json" | tee "${GITHUB_WORKSPACE}/plan2.json")
 echo "::set-output name=plan_json::${PLAN_JSON}"
